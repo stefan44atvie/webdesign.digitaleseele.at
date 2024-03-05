1cb993155d16fb0e6c0bb1bd5f0a4958e5cb39e3
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2024-03-05 14:13:59 +0100

    some changes

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="components/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Webdesign Digitale Seele</title>
 </head>
 <body>
 <nav id="main-navbar" class="navbar navbar-expand-md fixed-top navbar-before-scroll shadow-0">
--- weoffer.php
+++ weoffer.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="components/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Webdesign Digitale Seele</title>
 </head>
 <body>
 <nav id="main-navbar" class="navbar navbar-expand-md fixed-top navbar-before-scroll shadow-0">
@@ -54,13 +54,29 @@
 </nav>
 <!-- Navbar -->
 
-<section id="myoffer">
-    <p>
-    Mit unserer FullStack-Webentwicklung bei <span class="firma">Webdesign Digitale Seele</span> erhalten Sie nicht nur eine Website, sondern eine lebendige digitale Erfahrung. Von der Benutzeroberfläche bis zur Datenbank sorgen wir dafür, dass Ihre Online-Präsenz nicht nur funktioniert, sondern auch eine authentische digitale Seele besitzt.
+<section id="">
+  <div class="container introtext">
+    <p class="lead">
+      Mit unserer FullStack-Webentwicklung bei <span class="firma">Webdesign Digitale Seele</span> erhalten Sie nicht nur eine Website, sondern eine lebendige digitale Erfahrung. Von der Benutzeroberfläche bis zur Datenbank sorgen wir dafür, dass Ihre Online-Präsenz nicht nur funktioniert, sondern auch eine authentische digitale Seele besitzt.
     </p>
+  </div>
+</section>
+<section id="neuertest">
+  <h2 class="elegantshadow">Webdesign by Digitale Seele</h1>
 </section>
 
+<section id="text1">
+  <h1>
+    <span>We</span>
+    <div class="message">
+      <div class="word1">create</div>
+      <div class="word2">code</div>
+      <div class="word3">imaging</div>
+    </div>
+  </h1>
+</section>
+
+<script src="components/scripts/texteff2.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<!-- <script src="components/scripts/projectfile_state.js"></script> -->
 </body>
 </html>
