96ba7fc5c2062765b0960b0c27b753a3a3cfd782
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2024-03-11 18:49:33 +0100

    some tryouts

--- weoffer.php
+++ weoffer.php
@@ -32,16 +32,16 @@
             <a class="navbar-brand me-1" href="index.php"><img src="components/media/logo/D&S_Logo_200x115.jpg" height="20px" alt="Logo" loading="lazy" /></a>
 
             <li class="nav-item">
-            <a class="nav-link text-dark" href="weoffer.php">Was wir bieten</a>
+            <a class="nav-link text-dark" href="weoffer.php#startoffer">Was wir bieten</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link text-dark" href="vorteile.php">Ihre Vorteile</a>
+            <a class="nav-link text-dark" href="weoffer.php#vorteile">Ihre Vorteile</a>
             </li>
             <li class="nav-item">
             <a class="nav-link text-dark" href="https://webdesign.digitaleseele.at/portfolio">Mein Portfolio</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link text-dark" href="#ablauf">Der Ablauf</a>
+            <a class="nav-link text-dark" href="weoffer.php#ablauf">Der Ablauf</a>
             </li>
             <li class="nav-item">
             <a class="nav-link text-dark" href="#contactme">Kontakt</a>
@@ -55,7 +55,7 @@
 </nav>
 <!-- Navbar -->
 
-<section id="">
+<section id="startoffer">
   <div class="container-fluid introtext">
     
   </div>
@@ -72,6 +72,112 @@
     
   </div>
 </section>
+
+<section id="offers">
+    <p class="text_offers">
+        Ich spezialisiere mich darauf, moderne und responsive Webdesigns zu kreieren, die sich nahtlos an verschiedene Bildschirmgrößen anpassen und gleichzeitig ein ästhetisch ansprechendes Benutzererlebnis bieten. Von der Konzeption bis zur Umsetzung lege ich großen Wert darauf, dass Ihre Website nicht nur technisch ausgefeilt ist, sondern auch Ihre Markenidentität widerspiegelt und Ihre Besucher beeindruckt.
+    </p>
+    <p class="text_offers">
+        Ich biete ein ansprechendes Backend-Webdesign inklusive MySQL-Datenbankintegration an, das nicht nur die Funktionalität Ihrer Website unterstützt, sondern auch eine nahtlose Datenverwaltung ermöglicht. Durch eine benutzerfreundliche Schnittstelle und effiziente Datenbankstrukturen sorge ich dafür, dass Ihr Backend nicht nur professionell aussieht, sondern auch reibungslos funktioniert und Ihnen dabei hilft, Ihre Geschäftsprozesse optimal zu steuern.
+    </p>
+    <p class="text_offers">
+        Als Fullstack-Webdesigner biete ich umfassende Lösungen, die sowohl das Frontend als auch das Backend Ihrer Website abdecken. Von der Entwicklung ansprechender Benutzeroberflächen bis zur Implementierung robuster Backend-Systeme mit Datenbankintegration - mein Ziel ist es, ganzheitliche Webdesign-Lösungen anzubieten, die Ihren Anforderungen entsprechen und Ihre Online-Präsenz auf ein neues Niveau heben.
+    </p>
+</section>
+
+<section id="vorteile">
+    <div id="vorteilsintro_box">
+        <p class="text_vorteile">
+            Die Zusammenarbeit mit Webdesign Digitale Seele ist geprägt von Flexibilität und einem kundenorientierten Ansatz. Wir sind jederzeit erreichbar, um auf die Bedürfnisse unserer Kunden einzugehen und ihre Visionen in die Realität umzusetzen. Von der Planung bis zur Umsetzung stehen wir Ihnen zur Seite, um sicherzustellen, dass Ihr Projekt genau Ihren Wünschen entspricht und erfolgreich wird.
+        </p>
+    </div>
+    <div class="vorteile_intro">
+        <h4 class="intro">Die Zusammenarbeit mit Webdesign Digitale Seele bietet folgende Vorteile:</h4>
+    </div>
+    <div class="gridtestvorteil">
+        <div class="vor1pic">
+            <img src="components/media/logo/D&S_Logo_200x115.jpg">
+        </div>
+        <div class="vor1text">
+            <p class="text_vorteil">
+                Branchenkonforme Lösungen: Wir halten uns stets an die neuesten Branchenstandards und Trends, um sicherzustellen, dass unsere Lösungen den aktuellen Anforderungen entsprechen und unseren Kunden einen Wettbewerbsvorteil verschaffen
+            </p>
+        </div>
+        <div class="vor2pic">
+            <img src="components/media/logo/D&S_Logo_200x115.jpg">
+        </div>
+        <div class="vor2text">
+            <p class="text_vorteil">
+                Flexibilität und Kundenzufriedenheit: Wir legen großen Wert darauf, auf die individuellen Wünsche und Bedürfnisse unserer Kunden einzugehen und bieten flexible Lösungen, die ihre Erwartungen übertreffen
+            </p>
+        </div>
+        <div class="vor3pic">
+            <img src="components/media/logo/D&S_Logo_200x115.jpg">
+        </div>
+        <div class="vor3text">
+            <p class="text_vorteil">
+                Erfahrenes Expertenteam: Unser Team besteht aus erfahrenen Experten, die über das nötige Know-how verfügen, um innovative und ästhetisch ansprechende Designs zu entwickeln, die sowohl benutzerfreundlich als auch funktional sind
+            </p>
+        </div>
+        <div class="vor4pic">
+            <img src="components/media/logo/D&S_Logo_200x115.jpg">
+        </div>
+        <div class="vor4text">
+            <p class="text_vorteil">
+                Enge Zusammenarbeit: Wir arbeiten eng mit unseren Kunden zusammen, um sicherzustellen, dass jedes Projekt erfolgreich umgesetzt wird und die Ziele erreicht werden
+            </p>
+        </div>
+        <div class="vor5pic">
+            <img src="components/media/logo/D&S_Logo_200x115.jpg">
+        </div>
+        <div class="vor5text">
+            <p class="text_vorteil">
+                Branchenkonforme Lösungen: Wir halten uns stets an die neuesten Branchenstandards und Trends, um sicherzustellen, dass unsere Lösungen den aktuellen Anforderungen entsprechen und unseren Kunden einen Wettbewerbsvorteil verschaffen
+            </p>
+        </div>
+    </div>
+</section>
+
+<section id = "ablauf">
+    <div id ="ablauf_box">
+        <p class="text_ablauf"> 
+            Der Ablauf einer Zusammenarbeit mit uns bei Digitale Seele Webagentur ist darauf ausgerichtet, Ihnen einen reibungslosen und transparenten Prozess zu bieten:
+        </p>
+        <p class="text_ablauf">
+            1. **Jederzeit erreichbar**: Wir stehen Ihnen jederzeit zur Verfügung, um Ihre Fragen zu beantworten, Ihre Anliegen anzuhören und mit Ihnen gemeinsam an der Umsetzung Ihrer Vision zu arbeiten.
+        </p>
+        <p class="text_ablauf">
+            2. **Individuelle Lösungen für Ihre Website**: Wir verstehen, dass jede Website einzigartig ist. Daher entwickeln wir individuelle Lösungen, die Ihren spezifischen Anforderungen und Zielen entsprechen. Von der Gestaltung bis zur Funktionalität - wir gehen auf Ihre Wünsche ein.
+        </p>    
+        <p class="text_ablauf">
+            3. **Transparenz durch eigenes Kundenportal**: Über unser eigenes Kundenportal können Sie den Fortschritt Ihres Projekts jederzeit und von überall aus verfolgen. Sie haben die Möglichkeit, den aktuellen Stand einzusehen und bei Bedarf Dokumente herunterzuladen.
+        </p>
+        <p class="text_ablauf">
+            4. **Kundenzentrierter Ansatz**: Ihre Zufriedenheit liegt uns am Herzen. Deshalb nehmen wir uns die Zeit, Ihre Bedürfnisse genau zu verstehen und sicherzustellen, dass das Endprodukt genau Ihren Erwartungen entspricht. Ihre Wünsche und Anregungen fließen kontinuierlich in den Entwicklungsprozess ein.
+        </p>
+        <p class="text_ablauf">
+            Insgesamt streben wir danach, Ihnen nicht nur eine Website zu liefern, sondern eine maßgeschneiderte Lösung, die Ihr Unternehmen oder Ihre Marke bestmöglich repräsentiert und Ihren Erfolg im digitalen Raum fördert.
+        </p>
+    </div>
+
+</section>
+
+<section class="backpic">
+  <div id="backfrontpic_box">
+    <div id="pic_left">
+      <!-- <img src="components/media/back1.png">  -->
+    </div>
+    <div id="pic_right">AAA</div>
+  </div>
+</section>
+<div id="weknowit2">
+    <img src="components/media/front1.png"> 
+    <div id="test1"></div>
+</div>
+<div class="pictest1">
+  <img id="img1" src="components/media/front1.png"> 
+
+</div>
 
 <script src="components/scripts/texteff2.js"></script>
 <script src="components/scripts/effect2.js"></script>
